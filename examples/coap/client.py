f11e6f4ffec73035a8304ac09e04eac995904bc0
Felix Seidel <felix@seidel.me>
2018-05-31 12:26:19 +0200

    Add COAP example

---
+++ examples/coap/client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+
+from coapthon.client.helperclient import HelperClient
+
+host = "127.0.0.1"
+port = 5683
+path ="basic"
+
+with open('payload.jpg', 'r') as content_file:
+    client = HelperClient(server=(host, port))
+    content = content_file.read()
+    response = client.post(path, "test")
+    print response.pretty_print()
+    client.stop()
